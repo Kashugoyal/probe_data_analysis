fad28ab87eae6fa2a8aee3d6c980474ae3a2581f
Suhail Pallath Sulaiman <suhailsulaiman2018@u.northwestern.edu>
2018-04-30 12:36:31 -0500

    Code to read link data

--- map_matching.py
+++ map_matching.py
@@ -22,6 +22,14 @@ plt.show()
 with open('Partition6467LinkData.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
-        x.append(row[3])
-        y.append(row[4])
-        print 'latitude=',row[3],'longitude=', row[4]
+        # x.append(row[14])
+        # y.append(row[4])
+        a=  row[14].split("|")
+        # print a.split(",")
+
+        for lalo in a:
+            x1.append(lalo.split("/")[0])
+            y1.append(lalo.split("/")[1])
+            print lalo.split("/")[0],lalo.split("/")[1]
+plt.scatter(x, y)
+plt.show()
