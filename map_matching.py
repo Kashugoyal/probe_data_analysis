55dbeac0f15aa1ad5690f73fc432f2b2153bc878
Suhail Pallath Sulaiman <suhailsulaiman2018@u.northwestern.edu>
2018-05-04 22:22:29 -0500

    Complete code for reading and writing link and probedata into structuredarray of objects and to plot them

--- map_matching.py
+++ map_matching.py
@@ -19,13 +19,13 @@ link_data=[]
 #     i=0
 #     for row in spamreader:
 #
+#
 #         probe_obj=probe.Probe(row)
-#         probe_data.append(probe)
+#         probe_data.append(probe_obj)
 #         x.append(probe_obj.longitude)
 #         y.append(probe_obj.latitude)
 #
-#         print probe_obj.latitude
-#         print 'latitude=',probe_obj.latitude,'longitude=', probe_obj.longitude
+#         print 'No=',i,'latitude=',probe_obj.latitude,'longitude=', probe_obj.longitude
 #         i=i+1
 #         # if i>10000:
 #         #     break
@@ -37,10 +37,28 @@ link_data=[]
 #reading Link Data
 with open('Partition6467LinkData.csv', 'rb') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    i=0
     for row in spamreader:
+        # print 'row_length=',row[16]
         link_obj=link.Link(row)
-        link_data.append(link)
-        print link_obj.length
 
 
-print 'Hello'
+        for shape_point in link_obj.shape_points:
+            x.append(shape_point.longitude)
+            y.append(shape_point.latitude)
+            print 'No=',i,'latitude=', shape_point.latitude, 'longitude=', shape_point.longitude
+
+       #For displaying nodes only
+        # x.append(link_obj.reference_node.longitude)
+        # y.append(link_obj.reference_node.latitude)
+        # print 'No=', i, 'latitude=', link_obj.reference_node.latitude, 'longitude=', link_obj.reference_node.longitude
+        #
+        # x.append(link_obj.non_reference_node.longitude)
+        # y.append(link_obj.non_reference_node.latitude)
+        # print 'No=', i, 'latitude=', link_obj.non_reference_node.latitude, 'longitude=', link_obj.non_reference_node.longitude
+
+        i=i+1
+        link_data.append(link_obj)
+
+plt.scatter(x,y)
+plt.show()
